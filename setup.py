74a9e31f35f3b667f7b7298710dce6ac81c5d64d
Jordi Andreu <jandreu@cells.es>
2020-04-30 12:06:36 +0200

    Bump version 1.5.0 to 2.0.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages
 
 # The version is updated automatically with bumpversion
 # Do not update manually
-__version__ = '1.5.0'
+__version__ = '2.0.0'
 
 requirements = ['tango', 'click']
 
