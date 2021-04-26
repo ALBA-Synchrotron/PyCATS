b8c00786e2946e244587aa0422c28108d67e8033
Jordi Andreu <jandreu@cells.es>
2021-04-26 11:32:31 +0200

    Bump version 2.2.0 to 2.2.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages
 
 # The version is updated automatically with bumpversion
 # Do not update manually
-__version__ = '2.2.0'
+__version__ = '2.2.1'
 
 requirements = ['pytango', 'click']
 
